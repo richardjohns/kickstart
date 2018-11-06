9e80d1915cf2f619f728fe9a0aab394b288838ae
richardjohns <richard.johns193@gmail.com>
2018-11-06 20:10:28 +1300

    fix compile now gogo

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -3,23 +3,17 @@ const solc = require('solc');
 const fs = require('fs-extra');
 
 const buildPath = path.resolve(__dirname, 'build');
-// remoteSync looks at the path and deletes everything inside it for each compile
 fs.removeSync(buildPath);
 
 const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
-console.log('This is campaignPath: ',campaignPath);
-const source = fs.readFile(campaignPath, 'utf8');
+const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;
 
-// ensureDirSync checks if folder exists and creates if not there
 fs.ensureDirSync(buildPath);
 
-console.log('This is output: ', output);
 for (let contract in output) {
-    fs.outputJsonSync(
-        path.resolve(buildPath, contract + '.json'),
-        output[contract]
-    );
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(':', '') + '.json'),
+    output[contract]
+  );
 }
-
-console.log('This is source: ', source)
