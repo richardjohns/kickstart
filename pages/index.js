f3c0c59fcbb60ee39aa1546fe8789b3a99062b09
richardjohns <richard.johns193@gmail.com>
2018-11-07 22:23:02 +1300

    add semantic ui mods

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
-    async componentDidMount() {
+    // Can't use async componentDidMount to ask Next.js to call data from contract. 
+    // Need to use Next's getInitialProps. Next server executes just getInitialProps to load data without having 
+    // to render component which requires a lot of resources. 'static' attaches the props to the component class.
+    static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call()
-
-        console.log(campaigns)
+        return { campaigns }
     }
 
     render() {
-        return <div>Campaigns Index!</div>
+        return <div>{this.props.campaigns[0]}</div>
     }
 }
 
