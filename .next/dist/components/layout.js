fd0c1d236313534c536b2068bd1b6456106bd3f4
richardjohns <richard.johns193@gmail.com>
2018-11-10 23:03:59 +1300

    basic formage

--- .next/dist/components/layout.js
+++ .next/dist/components/layout.js
@@ -12,6 +12,10 @@ var _Header = require('./Header');
 
 var _Header2 = _interopRequireDefault(_Header);
 
+var _head = require('next/dist/lib/head.js');
+
+var _head2 = _interopRequireDefault(_head);
+
 var _semanticUiReact = require('semantic-ui-react');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -22,13 +26,17 @@ exports.default = function (props) {
     return _react2.default.createElement(_semanticUiReact.Container, {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 7
+            lineNumber: 8
         }
-    }, _react2.default.createElement(_Header2.default, {
+    }, _react2.default.createElement('link', { rel: 'stylesheet', href: '//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css', __source: {
+            fileName: _jsxFileName,
+            lineNumber: 9
+        }
+    }), _react2.default.createElement(_Header2.default, {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 8
+            lineNumber: 10
         }
     }), props.children);
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiSGVhZGVyIiwiQ29udGFpbmVyIiwicHJvcHMiLCJjaGlsZHJlbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQU8sQUFBWTs7OztBQUNuQixBQUFTLEFBRVQ7Ozs7OztrQkFBZSxVQUFBLEFBQUMsT0FBVSxBQUN0QjsyQkFDSSxBQUFDOztzQkFBRDt3QkFBQSxBQUNJO0FBREo7QUFBQSxLQUFBLGtCQUNJLEFBQUM7O3NCQUFEO3dCQURKLEFBQ0ksQUFDQztBQUREO0FBQUEsY0FGUixBQUNJLEFBRVcsQUFHbEI7QUFQRCIsImZpbGUiOiJMYXlvdXQuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL3JpY2hhcmRqb2hucy9wcm9qZWN0cy9raWNrc3RhcnQifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiSGVhZGVyIiwiSGVhZCIsIkNvbnRhaW5lciIsInByb3BzIiwiY2hpbGRyZW4iXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFPLEFBQVk7Ozs7QUFDbkIsQUFBTzs7OztBQUNQLEFBQVMsQUFFVDs7Ozs7O2tCQUFlLFVBQUEsQUFBQyxPQUFVLEFBQ3RCOzJCQUNJLEFBQUM7O3NCQUFEO3dCQUFBLEFBQ0k7QUFESjtBQUFBLEtBQUEsMENBQ1UsS0FBTixBQUFVLGNBQWEsTUFBdkIsQUFBNEI7c0JBQTVCO3dCQURKLEFBQ0ksQUFDQTtBQURBO3dCQUNBLEFBQUM7O3NCQUFEO3dCQUZKLEFBRUksQUFDQztBQUREO0FBQUEsY0FIUixBQUNJLEFBR1csQUFHbEI7QUFSRCIsImZpbGUiOiJMYXlvdXQuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL3JpY2hhcmRqb2hucy9wcm9qZWN0cy9raWNrc3RhcnQifQ==
--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import Head from 'next/head'
 import { Container } from 'semantic-ui-react'
 
 export default (props) => {
     return (
         <Container>
+            <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css"></link>
             <Header />
             {props.children}
         </Container>
