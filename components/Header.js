709b49a936b65191cc6c699e1d79072dab585e03
richardjohns <richard.johns193@gmail.com>
2018-11-18 22:48:37 +1300

    add header links so we can get around

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Menu } from 'semantic-ui-react'
+import { Link } from '../routes'
 
 export default () => {
     return (
         <div className='ui secondary menu' style={{ marginTop: '10px' }}>
-            <a className='active item'>CrowdCoin</a>
+            <Link route="/">
+                <a className='active item'>CrowdCoin</a>
+            </Link>
             <div className='right menu'>
                 <div className='item'>
                 <div className='ui icon input'>
@@ -12,7 +15,12 @@ export default () => {
                     <i aria-hidden='true' className='search icon' />
                 </div>
                 </div>
-                <a className='item'>Logout</a>
+                <Link route="/">
+                    <a className='active item'>Campaigns</a>
+                </Link>
+                <Link route="/campaigns/new">
+                    <a className='active item'>+</a>
+                </Link>
             </div>
         </div>
     )
