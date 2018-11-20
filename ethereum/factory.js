942b7baee35abf9e9bbb8b913cd8aa64ce8afe86
richardjohns <richard.johns193@gmail.com>
2018-11-20 20:51:24 +1300

    add new summary fn to contract and recompile and deploy

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,9 +2,10 @@ import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
 // instance has address of already deployed factory instance
+// save the new address of the factory if you re-compile and deploy. Then restart server to check factory can be interacted with.
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x00a71cAb97cC35e1A5470cE4B5af2061E3D3e592'
+    '0xFf78ac01eBDb557D32E3e6054D4e9ecc594E5Dd7'
 );
 
 export default instance;
