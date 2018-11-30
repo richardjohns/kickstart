daec96cb0917d5afe7c7bdcbe6c269dde65af78c
richardjohns <richard.johns193@gmail.com>
2018-11-30 20:42:14 +1300

    props to show

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import Layout from '../../components/Layout'
 
 class CampaignShow extends Component {
+    static async getInitialProps(props) {
+        console.log(props.query.address) // may only show in nextjs console
+
+        return {}
+    }
+
     render() {
         return (
         <Layout>
