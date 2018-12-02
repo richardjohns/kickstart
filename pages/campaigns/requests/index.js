7c5b626b634bfce06854667eb04f453a7c4f4f01
richardjohns <richard.johns193@gmail.com>
2018-12-02 23:58:48 +1300

    upgrade next due to security warning on git

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,77 +1,73 @@
-import React, { Component } from 'react'
-import { Form, Input, Message, Button, Table } from 'semantic-ui-react'
-import web3 from '../../../ethereum/web3'
-import Layout from '../../../components/Layout'
-import { Router, Link } from '../../../routes'
-import Campaign from '../../../ethereum/campaign'
-import RequestRow from '../../../components/RequestRow'
+import React, { Component } from 'react';
+import { Button, Table } from 'semantic-ui-react';
+import { Link } from '../../../routes';
+import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends Component {
-    state = {
-        value: '',
-        errorMessage: '',
-        loading: false
-    }
+  static async getInitialProps(props) {
+    const { address } = props.query;
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
 
-    static async getInitialProps(props) {
-        const { address } = props.query
-        const campaign = Campaign(address)
-        const requestCount = await campaign.methods.getRequestsCount().call() 
-        const approversCount = await campaign.methods.approversCount().call()
-
-        const requests = await Promise.all(
-            Array(requestCount).fill().map((element, index) => {
-                return campaign.methods.requests(index).call()
-            })
-        )
-        console.log('This is requests: ',requests)
-
-        return { address, requests, requestCount }
-    }
-
-    renderRows() {
-        return this.props.requests.map((request, index) => {
-            console.log('This is approvalCount: ',this.props.approvalCount)
-            return <RequestRow
-                        key={index}
-                        id={index}
-                        request={request}
-                        address={this.props.address}
-                        approversCount={this.props.approvalCount}
-                    />
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
         })
-    }
+    );
 
-    render() {
-        const { Header, Row, HeaderCell, Body } = Table
-        return (
-            <Layout>
-                <h3>Requests</h3>
-                <Link route={`/campaigns/${this.props.address}/requests/new`}>
-                    <a>
-                        <Button primary>Create New Request</Button>
-                    </a>
-                </Link>
-                <Table>
-                    <Header>
-                        <Row>
-                            <HeaderCell>ID</HeaderCell>
-                            <HeaderCell>Description</HeaderCell>
-                            <HeaderCell>Amount</HeaderCell>
-                            <HeaderCell>Recipient</HeaderCell>
-                            <HeaderCell>Approval Count</HeaderCell>
-                            <HeaderCell>Approve</HeaderCell>
-                            <HeaderCell>Finalise</HeaderCell>
+    return { address, requests, requestCount, approversCount };
+  }
 
-                        </Row>
-                    </Header>
-                    <Body>
-                        {this.renderRows()}
-                    </Body>
-                </Table>
-            </Layout>
-        )
-    }
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          id={index}
+          request={request}
+          address={this.props.address}
+          approversCount={this.props.approversCount}
+        />
+      );
+    });
+  }
+
+  render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
+    return (
+      <Layout>
+        <h3>Requests</h3>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary floated="right" style={{ marginBottom: 10 }}>
+              Add Request
+            </Button>
+          </a>
+        </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows()}</Body>
+        </Table>
+        <div>Found {this.props.requestCount} requests.</div>
+      </Layout>
+    );
+  }
 }
 
-export default RequestIndex
+export default RequestIndex;
